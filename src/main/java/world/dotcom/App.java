626006a2fbd155458953f095b486b3b5404dfc81
ram <dill@email.COM>
2022-06-28 23:22:06 +0530

    updated project

---
+++ src/main/java/world/dotcom/App.java
@@ -0,0 +1,13 @@
+package world.dotcom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
